6b80ce1e13f63a88e96a5c3fbc5b05c5b94fa834
lab_services_student <ST10199744>
2023-04-25 19:50:57 +0000

    Added a while loop with the menu

--- Dagada_Mukhethwa ST10199744/Program.cs
+++ Dagada_Mukhethwa ST10199744/Program.cs
@@ -13,6 +13,24 @@ namespace Dagada_Mukhethwa_ST10199744
             Recipe recipe = new Recipe();
 
             bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1. Enter ingredients");
+                Console.WriteLine("2. Enter steps");
+                Console.WriteLine("3. Display recipe");
+                Console.WriteLine("4. Scale recipe");
+                Console.WriteLine("5. Reset recipe");
+                Console.WriteLine("6. Clear recipe");
+                Console.WriteLine("7. Exit");
+
+                Console.Write("\nEnter choice: ");
+                int choice = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
+        
+    
+
