12b04c9c854d5012954a2a84ce114249b5a7bb96
lab_services_student <ST10199744>
2023-04-25 20:24:20 +0000

    Added a enter ingredients method

--- Dagada_Mukhethwa ST10199744/Recipess.cs
+++ Dagada_Mukhethwa ST10199744/Recipess.cs
@@ -16,5 +16,19 @@ namespace Dagada_Mukhethwa_ST10199744
             _steps = null;
         }
 
-    }
+        public void EnterIngredients()
+        {
+            Console.Write("Enter number of ingredients: ");
+            int numIngredients = int.Parse(Console.ReadLine());
+            _ingredients = new Ingredient[numIngredients];
+
+            for (int i = 0; i < numIngredients; i++)
+            {
+                Console.Write("Enter ingredient name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter quantity: ");
+                decimal quantity = decimal.Parse(Console.ReadLine());
+                Console.Write("Enter unit of measurement: ");
+                string unit = Console.ReadLine();
+            }
 }
