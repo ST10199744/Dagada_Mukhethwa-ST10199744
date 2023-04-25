944f75c44ed8a48d8f820bd3545f51ae8a2b7bea
lab_services_student <ST10199744>
2023-04-25 20:17:27 +0000

    Added a class called recipess

---
+++ Dagada_Mukhethwa ST10199744/Recipess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dagada_Mukhethwa_ST10199744
+{
+    internal class Recipess
+    {
+
+    }
+}
